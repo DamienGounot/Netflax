1ecb1b271fcebd9e48bfcaa349138f15f2272aa6
Damien <gounot@et.esiea.fr>
2021-08-23 16:34:02 +0200

    Initial config

---
+++ src/components/HomePage.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+    <p>Work in progress...</p>
+  </v-container>
+</template>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  devServer: {
+    public: 'http://www.gounot.org',
+    port: 80
+}
+}
