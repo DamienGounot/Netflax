ba667fb0a51559ec97aaf6f68f104f5125bd07ec
Damien <gounot@et.esiea.fr>
2021-08-27 23:23:27 +0200

    cipher password in DBB and manipulate cipher

--- Controller/login.php
+++ Controller/login.php
@@ -2,6 +2,7 @@
 
 include_once '../System/global_var.php';
 include_once '../System/log.php';
+include_once '../System/crypto.php';
 
 session_start();
 $connect = new PDO("mysql:host=".$HOST_DB.";dbname=".$NAME_DB, "$USERNAME_DB", "$PASSWORD_DB");
@@ -14,21 +15,34 @@ $password = $received_data->{'passwordLogin'};
 
 if(isset($username) && isset($password)){ // if every attempt data have been send
 
-    $query = "SELECT username FROM users WHERE username = '".$username."' AND password = '".$password."'";
+
+    $query = "SELECT password FROM users WHERE username = '".$username."'";
     $statement = $connect->prepare($query);
     $statement->execute();
     while($row = $statement->fetch(PDO::FETCH_ASSOC))
     {
         $data[] = $row;
-    }
+    }  
+        
+        
+    if(!empty($data)){ // if user exist
+        $storedPassword = $data[0]['password'];
 
-    if(!empty($data)){
-        $_SESSION['ONLINE'] = TRUE;
-        logger("[SUCCESS][LOGIN]".$username,$FILEPATH);
+        if(password_verify($password,$storedPassword)){ // match de password
+            $_SESSION['ONLINE'] = TRUE;
+            $_SESSION['USERNAME'] = $username;
+            logger("[SUCCESS][LOGIN]".$username,$FILEPATH);
+        }else{
+            logger("[ERROR][LOGIN]".$username." wrong password",$FILEPATH);
+            session_destroy();
+        }
     }else{
-        logger("[ERROR][LOGIN]".$username,$FILEPATH);
+        logger("[ERROR][LOGIN]".$username." does not exist",$FILEPATH);
         session_destroy();
     }
+
+
+
 
 }else{
     echo "Error when submitting data to server";
