db5d9c0a522c9290759679d53efb0340524937d9
Damien <gounot@et.esiea.fr>
2021-08-27 23:24:42 +0200

    add domain and noreply for email

--- System/global_var.php
+++ System/global_var.php
@@ -4,4 +4,6 @@ $HOST_DB = "localhost";
 $USERNAME_DB = "user";
 $PASSWORD_DB = "DNMfWlUEnbqUQ97yO9GvqS5D5B8n9dIB";
 $NAME_DB = "mydb";
+$NOREPLY = "no-reply@netflax.fr";
+$DOMAIN = "https://www.netflax.fr/";
 ?>
